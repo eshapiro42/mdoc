0997f16ad48366f399ebd59ee052b347534354ec
Eric Shapiro <eshapiro42@gmail.com>
2018-07-19 02:50:12 +0000

    Added MANIFEST.in so sdist would include the readme.

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mdoc",
-    version="0.0.6",
+    version="0.0.7",
     author="Eric Shapiro",
     author_email="eshapiro42@gmail.com",
     description="Modular documents",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    include_package_data = True,
     url="https://github.com/eshapiro42/mdoc",
     packages=setuptools.find_packages(),
     install_requires=['pathlib'],
