b4e70931b8c4e43862af6bb99e341d44515369b5
Eric Shapiro <eshapiro42@gmail.com>
2018-07-19 01:44:15 +0000

    Added static option to avoid parsing include files.

--- mdoc/mdoc.py
+++ mdoc/mdoc.py
@@ -1,11 +1,13 @@
+from __future__ import print_function
 from pathlib import Path
+import argparse
 import json
 import re
 import textwrap
 
 class MDoc(object):
 
-    def __init__(self, input_path=None, input_str=None, variables={}, showvariables=False):
+    def __init__(self, input_path=None, input_str=None, variables={}, showvariables=False, static=False):
         self.showvariables = showvariables
         self.variables = variables
         if input_path is not None:
@@ -22,8 +24,9 @@ class MDoc(object):
         # Delete any blank lines at the end
         while self.parsed[-1] == '\n':
             self.parsed = self.parsed[:-1]
-        # Parse the input into self.parsed
-        self.parse(showvariables)
+        if not static:
+            # Parse the input into self.parsed
+            self.parse(showvariables)
 
     # {mdoc tag read}
     def read(self):
@@ -32,23 +35,23 @@ class MDoc(object):
     # {mdoc untag read}
 
     def parse(self, showvariables=False):
-        self.parse_tag_includes()
-        self.parse_includes()
         if not showvariables:
             self.parse_variables()
         else:
             self.find_variables()
+        self.parse_tag_includes()
+        self.parse_includes()
 
     def parse_variables(self):
         variable_regex = re.compile('{mdoc (?!include)(.*?)}')
         self.parsed = re.sub(variable_regex, self.sub_variable, self.parsed)
 
     def parse_includes(self):
-        include_regex = re.compile('{mdoc include (?!tag)(.*?)}')
+        include_regex = re.compile('{mdoc include (?!tag)(.*?)(static)?}')
         self.parsed = re.sub(include_regex, self.sub_include, self.parsed)
 
     def parse_tag_includes(self):
-        tag_include_regex = re.compile('{mdoc include tag (.*?) from (.*?)}')
+        tag_include_regex = re.compile('{mdoc include tag (.*?) from (.*?)(static)?}')
         self.parsed = re.sub(tag_include_regex, self.sub_tag_include, self.parsed)
 
     def sub_variable(self, match):
@@ -59,11 +62,20 @@ class MDoc(object):
 
     def sub_include(self, match):
         include_path = match.group(1).strip()
-        try:
-            include_mdoc = MDoc(input_path=include_path, variables=self.variables, showvariables=self.showvariables)
-        except IOError:
-            raise LookupError('The include file {0} was not found but was requested by {1}'.format(include_path, self.input_path))
-        return include_mdoc.parsed
+        if match.group(2) is not None:
+            try:
+                with Path(include_path).open() as f:
+                    include_str = f.read()
+            except IOError:
+                raise LookupError('The include file {0} was not found but was requested by {1}'.format(include_path, self.input_path))
+            ret = include_str
+        else:
+            try:
+                include_mdoc = MDoc(input_path=include_path, variables=self.variables, showvariables=self.showvariables)
+            except IOError:
+                raise LookupError('The include file {0} was not found but was requested by {1}'.format(include_path, self.input_path))
+            ret = include_mdoc.parsed
+        return ret
 
     def sub_tag_include(self, match):
         tag_str = match.group(1).strip()
@@ -71,7 +83,7 @@ class MDoc(object):
         tag_regex = re.compile('{{mdoc tag {}}}'.format(tag_str))
         untag_regex = re.compile('{{mdoc untag {}}}'.format(tag_str))
         # Read in the file
-        with include_path.open() as f:
+        with Path(include_path).open() as f:
             f_str = f.read()
         # Look for the tag and untag and get their indices
         tag_match = re.search(tag_regex, f_str)
@@ -89,9 +101,13 @@ class MDoc(object):
         tag_contents = tag_contents[:last_newline_idx]
         # Remove any shared leading indents
         tag_contents = textwrap.dedent(tag_contents)
-        tag_mdoc = MDoc(input_str=tag_contents, variables=self.variables, showvariables=self.showvariables)
-        tag_mdoc.input_path = include_path
-        return tag_mdoc.parsed
+        if match.group(3) is not None:
+            ret = tag_contents
+        else:
+            tag_mdoc = MDoc(input_str=tag_contents, variables=self.variables, showvariables=self.showvariables)
+            tag_mdoc.input_path = include_path
+            ret = tag_mdoc.parsed
+        return ret
 
     def find_variables(self):
         variable_regex = re.compile('{mdoc (?!include)(.*?)}')
@@ -99,3 +115,42 @@ class MDoc(object):
         for var in var_list:
             self.variables[var] = ''
         self.parsed = json.dumps(self.variables, indent=2)
+
+def get_files():
+    parser = argparse.ArgumentParser()
+    # Command line arguments
+    parser.add_argument('--input', '-i', required=True, help='Input file')
+    output_group = parser.add_mutually_exclusive_group(required=True)
+    output_group.add_argument('--output', '-o', help='Output file')
+    output_group.add_argument('--dryrun', '-d', action='store_true', help='Print output to the screen only')
+    output_group.add_argument('--showvariables', '-s', action='store_true', help='Print out only which variables are referenced')
+    parser.add_argument('--variables', '-v', help='JSON file containing variable definitions')
+    args = parser.parse_args()
+    # Get input file name
+    input_file = Path(args.input)
+    # Get output file name
+    if args.output:
+        output_file = Path(args.output)
+    elif args.dryrun:
+        output_file = -1
+    elif args.showvariables:
+        output_file = -2
+    if args.variables:
+        with open(args.variables, 'r') as f:
+            variables = json.load(f)
+    else:
+        variables = {}
+    return input_file, output_file, variables
+
+def console():
+    input_file, output_file, variables = get_files()
+    if output_file == -2:
+        showvariables = True
+    else:
+        showvariables = False
+    mdoc = MDoc(input_path=input_file, variables=variables, showvariables=showvariables)
+    if output_file is -1 or output_file == -2:
+        print(mdoc.parsed)
+    else:
+        with open(output_file, 'w') as f:
+            f.write(mdoc.parsed)
