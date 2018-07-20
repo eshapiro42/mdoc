06ec38debefd6b279c13f642cc1114cebf3e3f77
eshapiro42 <eshapiro42@users.noreply.github.com>
2018-07-20 11:37:12 -0700

    Added examples

---
+++ examples/include/build.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+mdoc -i include.mdoc -d -v vars.json
---
+++ examples/snippet/hello_world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+   // {mdoc snip printf}
+   printf("Hello, World!");
+   // {mdoc unsnip printf}
+   return 0;
+}
